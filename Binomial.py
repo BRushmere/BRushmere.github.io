69f8766dec9a72910e0dff234fef3f43025e7eca
BRushmere <42934785+BRushmere@users.noreply.github.com>
2020-05-27 15:19:36 +0200

    Delete Binomial.py

--- Binomial.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from math import factorial as fac
-
-def manual_pmf(p, n, k):
-    return fac(n) / (fac(k) * fac(n - k)) * p**k * (1-p)**(n-k)
-
-print(manual_pmf(p=0.25, n=3, k=2))
